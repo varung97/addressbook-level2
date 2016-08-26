7259b20a33b2472867ef4294afa21d0da560a80b
varung97 <varung97@yahoo.co.in>
2016-08-26 21:30:16 +0800

    Bug fix in TextUI (caused due to attempt to access a private variable)

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -13,7 +13,8 @@ public class CommandResult {
     /** The feedback message to be shown to the user. Contains a description of the execution result */
     private final String feedbackToUser;
 
-    /** The list of persons that was produced by the command */
+
+	/** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;
 
     public CommandResult(String feedbackToUser) {
@@ -32,5 +33,11 @@ public class CommandResult {
     public Optional<List<? extends ReadOnlyPerson>> getRelevantPersons() {
         return Optional.ofNullable(relevantPersons);
     }
-
+    
+    /**
+     * @return feedbackToUser class variable
+     */
+    public String getFeedbackToUser() {
+    	return feedbackToUser;
+    }
 }
