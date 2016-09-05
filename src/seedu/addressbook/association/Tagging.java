1726565f4d11b1a848b0696ecece2818e27ab075
varung97 <varung97@yahoo.co.in>
2016-09-05 19:56:41 +0800

    Add association class Tagging

---
+++ src/seedu/addressbook/association/Tagging.java
@@ -0,0 +1,34 @@
+package seedu.addressbook.association;
+
+import seedu.addressbook.data.person.Person;
+import seedu.addressbook.data.tag.Tag;
+
+/**
+ * Association class between Person and Tags to represent tags that are added or
+ * deleted during a certain session
+ * 
+ * @author varungupta
+ *
+ */
+public class Tagging {
+	private enum Type {
+		ADDED, DELETED
+	};
+
+	public final Type type;
+	public final Person person;
+	public final Tag tag;
+
+	public Tagging(Type type, Person person, Tag tag) {
+		this.type = type;
+		this.person = person;
+		this.tag = tag;
+	}
+
+	@Override
+	public String toString() {
+		return type == Type.ADDED ? "+" : "-" + " "
+			   + person.getName() + " "
+			   + tag.tagName;
+	}
+}
